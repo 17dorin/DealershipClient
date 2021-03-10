759bf040f5d8ce08a1b73efc69cf2d2928c27301
Nick D'Oria <17dorin@gmail.com>
2021-03-09 23:15:59 -0500

    Finished project

---
+++ Controllers/CarController.cs
@@ -0,0 +1,76 @@
+﻿using DealershipClient.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DealershipClient.Controllers
+{
+    public class CarController : Controller
+    {
+        private CarDAL carInv = new CarDAL();
+
+        public IActionResult Index()
+        {
+            List<Car> inv = carInv.GetInventory();
+
+            return View(inv);
+        }
+
+        public IActionResult Search()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Search(int year, string make, string model, string color)
+        {
+            //Handles setting the year search parameter to a string, alternatively could alter your table to have year as a string
+            string yearSearch;
+            if(year != 0)
+            {
+                yearSearch = year.ToString();
+            }
+            else
+            {
+                yearSearch = null;
+            }
+
+            //Creates a dictionary where the key is the API endpoint, and the value is the parameter to search that endpoint with
+            Dictionary<string, string> searches = new Dictionary<string, string>();
+            searches.Add("year", yearSearch);
+            searches.Add("make", make);
+            searches.Add("model", model);
+            searches.Add("color", color);
+
+            //Parses dictionary down to only key value pairs where the value != null
+            Dictionary<string, string> notNullSearches = searches.Where(x => x.Value != null).ToDictionary(x => x.Key, x=> x.Value);
+
+            //Gets initial list of cars based on first item in search dictionary
+            List<Car> inv = new List<Car>();
+            if(notNullSearches.Count != 0)
+            {
+                inv = carInv.GetInventory(notNullSearches.First().Key, notNullSearches.First().Value);
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+
+            //Removes item from dictionary
+            notNullSearches.Remove(notNullSearches.First().Key);
+
+            //Performs LINQ statements on inventory list to parse it down, if any search items remain in the dictionary
+            if(notNullSearches.Count != 0)
+            {
+                foreach(KeyValuePair<string, string> kvp in notNullSearches)
+                { 
+                    inv = inv.Where(x => x.ToString().ToLower().Contains(kvp.Value)).ToList();
+                }
+            }
+
+            return View("Index", inv);
+        }
+    }
+}
---
+++ Models/CarDAL.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace DealershipClient.Models
+{
+    public class CarDAL
+    {
+        public string GetData()
+        {
+            string url = @"https://localhost:44390/api/cars";
+
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+            string json = rd.ReadToEnd();
+
+            return json;
+        }
+
+        public string GetData(string option, string search)
+        {
+            string url = $@"https://localhost:44390/api/cars/{option}/{search}";
+
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+            string json = rd.ReadToEnd();
+
+            return json;
+        }
+
+        public List<Car> GetInventory()
+        {
+            string json = GetData();
+
+            List<Car> inv = JsonConvert.DeserializeObject<List<Car>>(json);
+
+            return inv;
+        }
+
+        public List<Car> GetInventory(string option, string search)
+        {
+            string json = GetData(option, search);
+
+            List<Car> inv = JsonConvert.DeserializeObject<List<Car>>(json);
+
+            return inv;
+        }
+    }
+}
